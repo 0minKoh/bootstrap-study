f8453e8b345af081437cd504dc554f156c811e34
0minKoh <esesang13@naver.com>
2022-03-24 11:09:58 +0900

    툴팁

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const modalEl = document.querySelector('#exampleModal')
 modalEl.addEventListener('shown.bs.modal', function () {
   emailInputEl.focus()
 })
+
+// 툴팁 초기화
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
