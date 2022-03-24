cbe7ad67b5d4672eab2251962de6d3246d9b72b8
0minKoh <esesang13@naver.com>
2022-03-24 11:31:34 +0900

    npm 프로젝트 관리

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const emailInputEl = document.querySelector('#exampleInputEmail1')
-const modalEl = document.querySelector('#exampleModal')
-
-modalEl.addEventListener('shown.bs.modal', function () {
-  emailInputEl.focus()
-})
-
-// 툴팁 초기화
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
